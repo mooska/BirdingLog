024ea104a1979a6a6ded2a3457d809286d482375
Ryan Atkins <darthmooska@gmail.com>
2016-07-27 11:12:22 -0400

    Added multiple pages and MVC. Updated the home UI design.

--- src/BirdingLog/Controllers/Web/AppController.cs
+++ src/BirdingLog/Controllers/Web/AppController.cs
@@ -1,4 +1,7 @@
+using BirdingLog.Services;
+using BirdingLog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +12,51 @@ namespace BirdingLog.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        public IActionResult ProfileInfo()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("darthmooska@gmail.com"))
+            {
+                ModelState.AddModelError("", "Why would you message yourself?");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, model.Name, model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+
             return View();
         }
     }
--- src/BirdingLog/wwwroot/resources/js/scripts.js
+++ src/BirdingLog/wwwroot/resources/js/scripts.js
@@ -20,3 +20,4 @@ function gobackButtonPress(button, hideDiv) {
         });
     });
 }
+
